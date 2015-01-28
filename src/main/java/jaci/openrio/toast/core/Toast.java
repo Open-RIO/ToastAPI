c0fd0fa67166778a3ec5c266e4320bc42c815346
Jaci Brunning <jaci.brunning@gmail.com>
2015-01-29 05:56:00 +0800

    Commit gradle files

---
+++ src/main/java/jaci/openrio/toast/core/Toast.java
@@ -0,0 +1,12 @@
+package jaci.openrio.toast.core;
+
+import edu.wpi.first.wpilibj.RobotBase;
+
+public class Toast extends RobotBase {
+
+    @Override
+    public void startCompetition() {
+
+    }
+
+}
