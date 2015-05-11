a736ad4cacd890f67116c7461930e7f562aca4ea
JacisNonsense <jaci.brunning@gmail.com>
2015-05-11 22:36:22 +0800

    Obviously the most important part of the entire API

--- src/main/java/jaci/openrio/toast/core/thread/HeartbeatListener.java
+++ src/main/java/jaci/openrio/toast/core/thread/HeartbeatListener.java
@@ -7,6 +7,8 @@ package jaci.openrio.toast.core.thread;
  *
  * Functions inside of this interface should be Thread-Safe
  *
+ * Also known as the 'Stethoscope' class
+ * 
  * @author Jaci
  */
 public interface HeartbeatListener {
