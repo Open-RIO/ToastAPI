ae5a90be70c2baf852805ff0f80ffa339eb4dda2
JacisNonsense <jaci.brunning@gmail.com>
2015-06-11 14:17:44 +0800

    Update Security Manager to include Port warnings

--- src/main/java/jaci/openrio/toast/core/security/ToastSecurityManager.java
+++ src/main/java/jaci/openrio/toast/core/security/ToastSecurityManager.java
@@ -3,7 +3,7 @@ package jaci.openrio.toast.core.security;
 import jaci.openrio.toast.core.ToastBootstrap;
 
 import java.io.FilePermission;
-import java.nio.file.Files;
+import java.net.SocketPermission;
 import java.nio.file.InvalidPathException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -35,10 +35,16 @@ public class ToastSecurityManager extends SecurityManager {
         if (perm instanceof FilePermission) {
             FilePermission fp = (FilePermission) perm;
             h_File(fp);
+        } else if (perm instanceof SocketPermission) {
+            SocketPermission sp = (SocketPermission) perm;
+            h_Socket(sp);
         }
     }
 
     /** FILE PERMISSIONS **/
+    /* Disclaimer: By default, we block access to any files outside of the Toast Home directory (/home/lvuser/toast). This is
+     * so modules don't maliciously delete, write or execute anything that may be sensitive to users. Keep in mind, this is only
+     * for delete, write and execute permissions, and reading is still permitted. This is changed at the user's choice in the config */
 
     private Pattern fileDeniedAction = Pattern.compile(".*(delete|execute|write).*");
     private Pattern exceptionFiles = Pattern.compile(".*(AppData\\\\Local\\\\Temp\\\\).*");
@@ -47,20 +53,51 @@ public class ToastSecurityManager extends SecurityManager {
         if (fileDeniedAction.matcher(perm.getActions()).matches()) {
             try {
                 Path p = Paths.get(path);
-                if (!p.toAbsolutePath().startsWith(ToastBootstrap.toastHome.getAbsoluteFile().toPath()) && !isException(p.toAbsolutePath().toString())) {
+                if (!p.toAbsolutePath().startsWith(ToastBootstrap.toastHome.getAbsoluteFile().toPath()) && !isFileException(p.toAbsolutePath().toString())) {
                     SecurityPolicy.get().trigger(perm, "File Operation Violation, outside of Toast Scope: " + perm.getName());
                 }
             } catch (InvalidPathException e) {
-                if (!path.startsWith(ToastBootstrap.toastHome.getAbsolutePath()) && !isException(path)) {
+                if (!path.startsWith(ToastBootstrap.toastHome.getAbsolutePath()) && !isFileException(path)) {
                     SecurityPolicy.get().trigger(perm, "File Operation Violation, outside of Toast Scope: " + perm.getName());
                 }
             }
         }
     }
-    private boolean isException(String filepath) {
+    private boolean isFileException(String filepath) {
         String tmpdir = System.getProperty("java.io.tmpdir");
         if (tmpdir != null && filepath.startsWith(tmpdir)) return true;
         return exceptionFiles.matcher(filepath).matches();
+    }
+
+    /** SOCKET PERMISSIONS **/
+    /* Disclaimer: We don't block the sockets, we just warn the user when it's not on a port that FMS can forward, so they don't pull their hair out at competition
+     * when their sockets perms are disallowed */
+
+    private String[] exceptionPorts = new String[] {
+            "1735",   //NetworkTables
+    };
+    public void h_Socket(SocketPermission perm) {
+        if (perm.getActions().contains("listen")) {
+            SocketPermission impliee = new SocketPermission("*:5800-5810", "listen");
+            if (!impliee.implies(perm) && !isPortException(perm)) {
+                switch (SecurityPolicy.get()) {
+                    case NONE:
+                        break;
+                    default:
+                        SecurityPolicy.log().warn("Socket Port Warning, not within 5800...5810 (FMS will not forward it!): " + perm.getName());
+                        SecurityPolicy.log().exception(new Throwable());
+                        break;
+                }
+            }
+        }
+    }
+    private boolean portExceptionCheck(SocketPermission permission, String p) {
+        SocketPermission impliee = new SocketPermission("*:" + p, "listen");
+        return impliee.implies(permission);
+    }
+    private boolean isPortException(SocketPermission perm) {
+        for (String p : exceptionPorts) if (portExceptionCheck(perm, p)) return true;
+        return false;
     }
 
 }
