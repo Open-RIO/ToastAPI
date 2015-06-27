ca525c90e4805d35de0cda063e9b25656941017b
JacisNonsense <jaci.brunning@gmail.com>
2015-06-28 00:36:52 +0800

    Setup the Inspector for Toast Buils

---
+++ tools/inspector.rb
@@ -0,0 +1,15 @@
+require 'gist'
+require 'json'
+exported_libs = Dir["build/libs/Toast-*.jar"]
+@data = { :files => {} }
+exported_libs.each do |file_name|
+  if file_name =~ /.*Toast-(.*)-raw.jar/
+    @data[:files][:raw] = file_name
+  elsif file_name =~ /.*Toast-(.*)-sources.jar/
+    @data[:files][:sources] = file_name
+  elsif file_name =~ /.*Toast-(.*).jar/
+    @data[:files][:jar] = file_name
+  end
+end
+@data[:sizes] = @data[:files].map { |type, name| {type => File.size(name)} }.reduce(:merge)
+puts "Inspector Complete: #{Gist.gist("#{JSON.generate(@data)}")["html_url"]}"
