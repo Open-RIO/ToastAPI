b15ef4d7e98899e77ed6b044b2bee680b153a7cb
JacisNonsense <jaci.brunning@gmail.com>
2015-07-05 20:49:07 +0800

    Use a non-rate-limited Gist URL

--- tools/inspector.rb
+++ tools/inspector.rb
@@ -1,16 +1,25 @@
 # Reports information about FileSize and File Locations
 require 'gist'
 require 'json'
-exported_libs = Dir["build/libs/Toast-*.jar"]
-@data = { :files => {} }
-exported_libs.each do |file_name|
-  if file_name =~ /.*Toast-(.*)-raw.jar/
-    @data[:files][:raw] = file_name
-  elsif file_name =~ /.*Toast-(.*)-sources.jar/
-    @data[:files][:sources] = file_name
-  elsif file_name =~ /.*Toast-(.*).jar/
-    @data[:files][:jar] = file_name
+begin
+  if ENV["TRAVIS_PULL_REQUEST"] == "false"
+    exported_libs = Dir["build/libs/Toast-*.jar"]
+    @data = { :files => {} }
+    exported_libs.each do |file_name|
+      if file_name =~ /.*Toast-(.*)-raw.jar/
+        @data[:files][:raw] = file_name
+      elsif file_name =~ /.*Toast-(.*)-sources.jar/
+        @data[:files][:sources] = file_name
+      elsif file_name =~ /.*Toast-(.*).jar/
+        @data[:files][:jar] = file_name
+      end
+    end
+    @data[:sizes] = @data[:files].map { |type, name| {type => File.size(name)} }.reduce(:merge)
+    puts "Metrics uploaded to Gist: #{Gist.gist(File.read("stats/metrics.json"), access_token: ENV['gist'])["html_url"], filename: "metrics.json"}"
+    puts "Inspector Complete: #{Gist.gist("#{JSON.generate(@data)}", access_token: ENV['gist'], filename: "inspector.json")["html_url"]}"
+  else
+    puts "Pull Request -- Metrics Disabled"
   end
+rescue
+  puts "Could not Upload Metrics :c"
 end
-@data[:sizes] = @data[:files].map { |type, name| {type => File.size(name)} }.reduce(:merge)
-puts "Inspector Complete: #{Gist.gist("#{JSON.generate(@data)}")["html_url"]}"
