74305f50545dfc80020373bed813567c85300579
Jaci R <jaci.brunning@gmail.com>
2015-07-13 04:01:32 +0800

    Update config example

--- examples/config/Configuration.java
+++ examples/config/Configuration.java
@@ -1,17 +1,17 @@
 package toast.examples;
 
-import jaci.openrio.toast.core.loader.groovy.GroovyPreferences;
+import jaci.openrio.toast.lib.module.ModuleConfig;
 
 public class Configuration {
 
-    GroovyPreferences preferences;
+    ModuleConfig preferences;
 
     public void register() {                // This would be called from your Main module
-        preferences = new GroovyPreferences("my_module_name");
-        int my_int = preferences.getInt("my_int", 0, "A standard integer for the module");
-        String my_str = preferences.getString("my_str", "Hello World", "A standard string for the module");
+        preferences = new ModuleConfig("my_module_name");
+        int my_int = preferences.getInt("my_int", 0);
+        String my_str = preferences.getString("my_str", "Hello World");
 
-        int nested_int = preferences.getInt("nested.my_int", 1, "An integer nested inside of 'nested'");
+        int nested_int = preferences.getInt("nested.my_int", 1);
     }
 
 }
