0447dc6b870397badb7252bac70cc05c53c1b078
LoadingPleaseWait <michael@murphey.org>
2016-03-10 22:26:38 -0600

    Add initial support for encoders in simulation closes #35

---
+++ patches/src/main/java/edu/wpi/first/wpilibj/hal/EncoderJNI.java
@@ -0,0 +1,57 @@
+package edu.wpi.first.wpilibj.hal;
+
+import java.nio.IntBuffer;
+
+/**
+ * JNIWrapper for encoders
+ */
+public class EncoderJNI extends JNIWrapper {
+
+    public static long initializeEncoder(byte port_a_module, int port_a_pin,
+              boolean port_a_analog_trigger, byte port_b_module, int port_b_pin, boolean port_b_analog_trigger,
+              boolean reverseDirection, IntBuffer index){
+        return 0;
+    }
+    
+    public static void freeEncoder(long encoder_pointer){
+    }
+    
+    public static void resetEncoder(long encoder_pointer){
+    }
+    
+    public static int getEncoder(long encoder_pointer){
+        return 0;
+    }
+    
+    public static double getEncoderPeriod(long encoder_pointer){
+        return 0;
+    }
+    
+    public static void setEncoderMaxPeriod(long encoder_pointer, double maxPeriod){
+    }
+    
+    public static boolean getEncoderStopped(long encoder_pointer){
+        return false;
+    }
+    
+    public static boolean getEncoderDirection(long encoder_pointer){
+        return false;
+    }
+    
+    public static void setEncoderReverseDirection(long encoder_pointer,
+              boolean reverseDirection){
+    }
+    
+    public static void setEncoderSamplesToAverage(long encoder_pointer,
+              int samplesToAverage){
+    }
+    
+    public static int getEncoderSamplesToAverage(long encoder_pointer){
+        return 0;
+    }
+    
+    public static void setEncoderIndexSource(long digital_port, int pin,
+              boolean analogTrigger, boolean activeHigh, boolean edgeSensitive){
+    }
+
+}
